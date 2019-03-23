30e245429ba020b1815e32068a7b99a17226f750
Nomeleel <choonlay@outlook.com>
2019-03-23 10:43:22 +0800

    Add Camera script

---
+++ New Unity Project/Assets/Script/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform playTransform;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - playTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = offset + playTransform.position;
+    }
+}
