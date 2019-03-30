84f2d1464f73c47e25a2a189393cf553bd394991
Nomeleel <choonlay@outlook.com>
2019-03-30 19:29:52 +0800

    localhost

--- New Unity Project/Assets/Script/Play.cs
+++ New Unity Project/Assets/Script/Play.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Play : MonoBehaviour
 {
 
     private Rigidbody rd;
+    private const string scoreTextFormat = "Score: {0}";
+    private int score;
 
-    public int seed = 5;
+    public Text scoreText;
+    public GameObject winText;
+    public int seed = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -22,4 +26,24 @@ public class Play : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         rd.AddForce(new Vector3(h, 0, v) * seed);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name.StartsWith("cube", StringComparison.InvariantCultureIgnoreCase))
+        {
+            print(collision.collider.name);
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name.StartsWith("cube", StringComparison.InvariantCultureIgnoreCase))
+        {
+            scoreText.text = string.Format(scoreTextFormat, ++score);
+            winText.SetActive(true);
+            Destroy(other.gameObject);            
+        }
+    }
+
 }
